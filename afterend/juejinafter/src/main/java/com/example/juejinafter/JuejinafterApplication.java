da6aaca220613726d92e76de05c49d5e2b506856
heerxin <1459096537@qq.com>
2022-08-16 11:06:08 +0800

    fix: init

--- afterend/juejinafter/src/main/java/com/example/juejinafter/JuejinafterApplication.java
+++ afterend/juejinafter/src/main/java/com/example/juejinafter/JuejinafterApplication.java
@@ -1,9 +1,14 @@
 package com.example.juejinafter;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan("com.example.juejinafter.mapper")
+@EnableTransactionManagement
 public class JuejinafterApplication {
 
     public static void main(String[] args) {
