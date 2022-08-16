1be5b2e808cdf5ef9fe0da558e58ccfd1daeb1e9
lyd11250 <1125025634@qq.com>
2022-08-16 21:22:41 +0800

    ✨ feat(前端-文章列表): 新增防抖函数
    - 防止滚动到接近底部时多次触发加载方法

--- frontend/juejin-frontend/src/views/articles/index.vue
+++ frontend/juejin-frontend/src/views/articles/index.vue
@@ -1,7 +1,10 @@
 <template>
     <CategoryHeader :categories="categories" pageName="articles" />
     <div class="article-list">
-        <el-skeleton class="skeleton" v-if="isLoading" animated>
+        <div v-for="a in articles" :key="a.id">
+            <ArticleCard :article="a" />
+        </div>
+        <el-skeleton class="skeleton" animated>
             <template #template>
                 <div style="display: flex;flex-direction: column;justify-content: space-between;">
                     <el-skeleton-item variant="p" style="width: 200px;height: 20px;" />
@@ -12,16 +15,14 @@
                 <el-skeleton-item variant="image" style="width: 200px; height: 150px;" />
             </template>
         </el-skeleton>
-        <div v-else v-for="a in articles" :key="a.id">
-            <ArticleCard :article="a" />
-        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import axios from 'axios';
-import { onMounted, onUnmounted, reactive, ref } from 'vue';
+import { onMounted, onUnmounted, reactive, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
+import Debounced from '@/hooks/useDebounced'
 import ArticleCard from './components/ArticleCard.vue';
 const categories = [
     { category: 'recommended', title: '综合' },
@@ -34,10 +35,9 @@ const categories = [
     { category: 'career', title: '代码人生' },
 ]
 const route = useRoute()
-const isLoading = ref(true)
 const articles: any = reactive([])
 const currentPage = ref(0)
-const pageSize = 20;
+const pageSize = 10;
 const getArticles = () => {
     axios.get('/articles/list', {
         params: {
@@ -49,10 +49,24 @@ const getArticles = () => {
         if (res.data.msg === 'success') {
             articles.push(...res.data.list)
             currentPage.value += 1
-            isLoading.value = false
         }
+
     })
 }
+
+watch(
+    () => route.params.category,
+    (newVal, oldVal) => {
+        // 当文章分类发生变化时
+        if (newVal != oldVal) {
+            // 清空原数组
+            articles.splice(0, articles.length)
+            // 重置当前页为0
+            currentPage.value = 0
+            // 重新获取文章列表
+            getArticles()
+        }
+    })
 
 const handleScroll = () => {
     // 距顶部
@@ -62,19 +76,25 @@ const handleScroll = () => {
     // 滚动条总高度
     var scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
     // 滚动到底部时加载列表
-    if (scrollTop + clientHeight === scrollHeight) {
+    console.log(scrollTop, clientHeight, scrollHeight);
+
+    if (scrollTop + clientHeight >= scrollHeight - 200) {
         getArticles()
     }
-
 }
 
+const debounced = new Debounced()
+const debouncedUse: Function = debounced.use(handleScroll, 500)
+const func = (...args: any) => {
+    debouncedUse(...args)
+}
 onMounted(() => {
     getArticles()
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener('scroll', func)
 })
 
 onUnmounted(() => {
-    window.removeEventListener('scroll', handleScroll)
+    window.removeEventListener('scroll', func)
 })
 </script>
 
